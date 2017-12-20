c03f90bdb40026af2094e8c4324b193fd427941c
Arthur Cayet <arthurcayet@hotmail.fr>
2017-12-20 12:07:25 +0100

    lol les merges

--- Projet AppServ Java Serv/src/services/ServiceEmprunt.java
+++ Projet AppServ Java Serv/src/services/ServiceEmprunt.java
@@ -1,6 +1,9 @@
 package services;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class ServiceEmprunt implements Runnable{
@@ -17,6 +20,18 @@ public class ServiceEmprunt implements Runnable{
 	@Override
 	public void run() {
 		System.out.println("Connexion reservation " + this.numero + "demarrée");
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+			
+			String numLivre = in.readLine();
+			
+			
+			String numAbonne = in.readLine();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 		
 		
 	}
